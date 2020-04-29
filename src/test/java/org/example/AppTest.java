708811259b6bc1e62a2444f7dcff6138cbda872e
nPaloukosQA <62283168+nPaloukosQA@users.noreply.github.com>
2020-04-29 10:44:21 +0300

    Made a simple test that searches for funny dog pictures

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,50 @@
 package org.example;
 
+import static java.lang.Thread.*;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.WeakHashMap;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void seleniumExampleTest() throws InterruptedException{
+        driver.manage().window().maximize();
+        sleep(2000);
+        driver.get("http://www.google.com");
+        sleep(1000);
+        WebElement googleSearchBar = driver.findElement(By.name("q"));
+        googleSearchBar.sendKeys("funny dog pics");
+        sleep(10000);
+        googleSearchBar.submit();
+        sleep(2000);
+        WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for funny dog"));
+        linkToPictures.click();
+        sleep(2000);
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
 
 }
